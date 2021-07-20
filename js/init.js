434ff7962c1120aa4230d9fdd3cb697568bd986c
Chat801JMV <82191893+JairIvan@users.noreply.github.com>
2021-07-20 14:40:52 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+ apiKey: "AIzaSyABxC7yVN5GPNC-8B-XcR73mQ-ms8mRqwY",
+    authDomain: "chat801jmv.firebaseapp.com",
+    projectId: "chat801jmv",
+    storageBucket: "chat801jmv.appspot.com",
+    messagingSenderId: "665797982096",
+    appId: "1:665797982096:web:71a4c4e1c0d1e481fe6515",
+    measurementId: "G-XHL8BVFZ0W"
+});
